eedd72048dbe7b15eab166b27c28d4b60150e729
김준호 <tuvmxm@naver.com>
2020-07-21 14:41:39 +0900

    :sparkles: Add 'SignupView'

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,29 +2,27 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
 
+import SignupView from "@/views/accounts/SignupView.vue";
+
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/accounts/signup",
+    name: "Signup",
+    component: SignupView,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;

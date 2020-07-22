d9f81b72579b310d557546a18f32183177ef5c83
김준호 <tuvmxm@naver.com>
2020-07-22 11:05:01 +0900

    :sparkles: Add Signup & Login component

---
+++ frontend/src/views/accounts/LoginView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign in</h1>
+          <p class="text-xs-center">
+            <router-link to="/register">Need an account?</router-link>
+          </p>
+          <!-- <ul class="error-messages">
+            <li>That email is already taken</li>
+          </ul> -->
+          <form>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                v-model="email"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                v-model="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LoginView extends Vue {
+  email = "";
+  password = "";
+
+  login() {
+    console.log("login with");
+  }
+}
+</script>
+
+<style></style>
---
+++ frontend/src/views/accounts/SignupView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>Signup</h1>
+    <div class="form-group">
+      <label for="username">아이디</label>
+      <input
+        v-model="signupData.username"
+        class="form-control"
+        id="username"
+        type="text"
+        placeholder="이메일을 입력하세요"
+      />
+    </div>
+    <div class="form-group">
+      <label for="password1">비밀번호</label>
+      <input
+        v-model="signupData.password1"
+        class="form-control"
+        id="password1"
+        type="password"
+        placeholder="비밀번호"
+      />
+    </div>
+    <div class="form-group">
+      <label for="password2">비밀번호 확인</label>
+      <input
+        v-model="signupData.password2"
+        class="form-control"
+        id="password2"
+        type="password"
+        placeholder="비밀번호 확인"
+      />
+    </div>
+    <div>
+      <button @click="signup(signupData)">Signup</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+import { mapActions } from "vuex";
+
+interface SignupData {
+  username: string | null;
+  password1: string | null;
+  password2: string | null;
+}
+
+@Component({
+  methods: {
+    ...mapActions(["signup"]),
+  },
+})
+@Component
+export default class SignupView extends Vue {
+  signupData: SignupData = {
+    username: null,
+    password1: null,
+    password2: null,
+  };
+}
+</script>
+
+<style></style>

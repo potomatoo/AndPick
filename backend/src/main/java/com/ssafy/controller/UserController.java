888c8cadfa342217df7e59014d520954de513769
ydh2338 <ydh2338@naver.com>
2020-07-23 21:49:59 +0900

    :sparkles: user sign up Closes S03P12B107-44

---
+++ backend/src/main/java/com/ssafy/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.ssafy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.model.BasicResponse;
+import com.ssafy.model.dto.User;
+import com.ssafy.model.repository.UserRepository;
+import com.ssafy.model.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	@Autowired
+	PasswordEncoder encoder;
+
+	@PostMapping("/api/public/signup")
+	public Object signup(@RequestParam(value = "user_id") String userId,
+			@RequestParam(value = "user_password") String userPassword,
+			@RequestParam(value = "user_name") String userName, @RequestParam(value = "user_type") int userType) {
+
+		User user = userService.Signup(new User(userId, encoder.encode(userPassword), userName, userType));
+
+		BasicResponse result = new BasicResponse();
+
+		ResponseEntity response = null;
+
+		if (user != null) {
+			result.status = true;
+			result.data = "Sign up success";
+			response = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result.status = false;
+			result.data = "중복된 이메일 입니다.";
+			response = new ResponseEntity<>(result, HttpStatus.CONFLICT);
+		}
+		return response;
+	}
+
+}
---
+++ backend/src/main/java/com/ssafy/model/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ssafy.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.model.dto.User;
+import com.ssafy.model.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User Signup(User model) {
+		// TODO Auto-generated method stub
+		System.out.println("Service");
+		if (userRepository.findByUserId(model.getUserId()) != null) {
+			return null;
+		}
+		return userRepository.save(model);
+	}
+
+	@Override
+	public User findByUser_id(String userId) {
+		// TODO Auto-generated method stub
+		return userRepository.findByUserId(userId);
+	}
+
+}

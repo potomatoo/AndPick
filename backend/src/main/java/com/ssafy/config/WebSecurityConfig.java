436f8b596bd7807d069217e229d37af800d4fe30
ydh2338 <ydh2338@naver.com>
2020-07-23 13:22:31 +0900

    :sparkles: set JWT,SpringSecurity

---
+++ backend/src/main/java/com/ssafy/config/WebSecurityConfig.java
@@ -0,0 +1,85 @@
+package com.ssafy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.ssafy.filter.JwtAuthenticationFilter;
+import com.ssafy.filter.JwtAuthorizationFilter;
+import com.ssafy.model.repository.UserRepository;
+import com.ssafy.security.UserPrincipalDetailService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserPrincipalDetailService userPrincipalDetailsService;
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+
+		return daoAuthenticationProvider;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.formLogin().disable();
+		http.csrf().disable()//rest api이므로 csrf보안이 필요없음
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//jwt 토큰 사용으로 세션이 필요 없음
+			.and()
+				.addFilterAt(customfilter(),UsernamePasswordAuthenticationFilter.class)
+				.addFilter(new JwtAuthorizationFilter(authenticationManager(), this.userRepository))
+				.authorizeRequests()
+					.antMatchers("/api/public/*").permitAll()
+					.anyRequest().authenticated();
+		
+		// 로그아웃 처리
+		http.logout().invalidateHttpSession(true).clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/api/user/logout")).logoutSuccessUrl("/index").permitAll();
+		// @formatter:on
+	}
+
+	protected JwtAuthenticationFilter customfilter() {
+		JwtAuthenticationFilter authfilter = new JwtAuthenticationFilter();
+		
+		try {
+			authfilter.setFilterProcessesUrl("/api/public/login");
+			authfilter.setAuthenticationManager(this.authenticationManagerBean());
+			authfilter.setUsernameParameter("user_id");
+			authfilter.setPasswordParameter("user_password");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		return authfilter;
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}

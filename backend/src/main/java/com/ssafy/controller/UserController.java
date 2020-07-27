c84c5cd592eebdb2f4b02730cf4eb462001a321c
ydh2338 <ydh2338@naver.com>
2020-07-27 10:42:28 +0900

    :art: Allow cros

--- backend/src/main/java/com/ssafy/controller/UserController.java
+++ backend/src/main/java/com/ssafy/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.ssafy.model.dto.User;
 import com.ssafy.model.repository.UserRepository;
 import com.ssafy.model.service.UserService;
 
+@CrossOrigin("*")
 @RestController
 public class UserController {
 

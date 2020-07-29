f86ea07c8fdd8d8af32c5185d01e423089312e5e
ydh2338 <ydh2338@naver.com>
2020-07-29 15:35:39 +0900

    :art: Add controller find by category and rss_name

--- backend/src/main/java/com/ssafy/controller/RssController.java
+++ backend/src/main/java/com/ssafy/controller/RssController.java
@@ -9,31 +9,31 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.google.gson.Gson;
 import com.ssafy.model.BasicResponse;
 import com.ssafy.model.dto.Rss;
 import com.ssafy.model.dto.RssChannel;
 import com.ssafy.model.dto.RssItem;
-import com.ssafy.model.dto.User;
 import com.ssafy.model.service.RssService;
 
 @Controller
 public class RssController {
 
 	@Autowired
-	RedisTemplate<String, Object> redisTemplate;
+	private RedisTemplate<String, Object> redisTemplate;
 	@Autowired
-	RssService rssService;
+	private RssService rssService;
 
-	@RequestMapping(value = "/api/public/test/rss" ,produces = "application/json; charset=utf8")
+	@RequestMapping(value = "/api/public/test/rss", produces = "application/json; charset=utf8")
 	public Object getRss() {
 		ResponseEntity response = null;
 		BasicResponse result = new BasicResponse();
 
-		List<Rss> rssList = rssService.searchAll();
+		List<Rss> rssList = rssService.findAll();
 		List<RssItem> rssItem = new LinkedList<RssItem>();
 
 		for (Rss item : rssList) {
@@ -41,19 +41,75 @@ public class RssController {
 			System.out.println(channel);
 			rssItem.addAll(channel.getItems());
 		}
-		
-		//sort과정
+
+		// sort과정
 		List<RssItem> resultItem = new ArrayList<RssItem>(10);
 
-		for(int i=0;i<10;i++) {
+		for (int i = 0; i < 10; i++) {
 			resultItem.add(rssItem.get(i));
 		}
-		
+
 		result.status = true;
-		result.message= "rss test api...";
+		result.message = "rss test api...";
 		result.data = resultItem;
-		
-		return new ResponseEntity<>(result,HttpStatus.OK);
+
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/api/rss/list/all")
+	public Object findAllRss(@RequestHeader("Authorization") String jwtToken) {
+		ResponseEntity response = null;
+		BasicResponse result = new BasicResponse();
+
+		result.status = true;
+		result.message = "모든 rss 정보입니다.";
+		result.data = rssService.findAll();
+
+		response = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return response;
+	}
+
+	@GetMapping(value = "/api/rss/list/category")
+	public Object findRssByCategoryName(@RequestHeader("Authorization") String jwtToken,
+			@RequestParam("categoryName") String categoryName) {
+		ResponseEntity response = null;
+		BasicResponse result = new BasicResponse();
+
+		result.data = rssService.findByCategoryName(categoryName);
+		result.status = (result.data != null) ? true : false;
+
+		if (result.status) {
+			result.message = "선택한 카테고리의 RSS리스트 입니다.";
+			response = new ResponseEntity<>(result, HttpStatus.OK);
+
+		} else {
+			result.message = "잘못된 카테고리 명입니다.";
+			response = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+
+		}
+
+		return response;
+	}
+
+	@GetMapping(value = "/api/rss/list/name")
+	public Object findRssByRssName(@RequestHeader("Authorization") String jwtToken,
+			@RequestParam("rssName") String rssName) {
+		ResponseEntity response = null;
+		BasicResponse result = new BasicResponse();
+
+		result.data = rssService.findByRssName(rssName);
+		result.status = (result.data != null) ? true : false;
+
+		if (result.status) {
+			result.message = "rss이름으로 검색 결과 입니다.";
+			response = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result.message = "해당하는 rss가 없습니다.";
+			response = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return response;
 	}
 
 }

9f914c9eaf5700493bec9bec7e56fa9e9b96424c
ydh2338 <ydh2338@naver.com>
2020-07-29 22:53:54 +0900

    :sparkles: Subscribe Service

--- backend/src/main/java/com/ssafy/model/service/SubscribeService.java
+++ backend/src/main/java/com/ssafy/model/service/SubscribeService.java
@@ -1,5 +1,24 @@
 package com.ssafy.model.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.model.dto.Subscribe;
+import com.ssafy.model.dto.User;
+
+@Service
 public interface SubscribeService {
+	public Subscribe saveSubscribe(User user, long feedId, String subscribeName, String rssUrl, String categoryName);
+
+	public Subscribe updateSubscribe(User user, long feedId, long subscribeId, String subscribeName);
+
+	public boolean deleteSubscribe(long subscribeId);
+
+	public List<Subscribe> findSubscribeByUser(User user);
+
+	public List<Subscribe> findSubscribeByFeed(long feedId);
+
+	public Subscribe findSubscribeBySubscribe(long subscribeId);
 
 }

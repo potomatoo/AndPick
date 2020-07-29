209abc55dad93c1255cc75772f71487b56a65bab
ydh2338 <ydh2338@naver.com>
2020-07-29 18:24:17 +0900

    :art: Delete subscribe when Feed deleted

--- backend/src/main/java/com/ssafy/model/repository/FeedRepository.java
+++ backend/src/main/java/com/ssafy/model/repository/FeedRepository.java
@@ -3,6 +3,9 @@ package com.ssafy.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.model.dto.Feed;
@@ -11,4 +14,8 @@ import com.ssafy.model.dto.Feed;
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 	public Feed findOneByFeedName(String feedName);
 	public List<Feed> findByUserNo(Long userNo);
+	
+	@Modifying
+	@Query(value = "delete from SSAFYDB.subscribe where feed_id=:feed_id ",nativeQuery = true)
+	public void deleteSubscribe(@Param("feed_id")Long feed_id);
 }

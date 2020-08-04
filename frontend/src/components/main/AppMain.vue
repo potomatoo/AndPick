9358d6e867e6fe4653dc01fd3d532b460cd3863b
김준호 <tuvmxm@naver.com>
2020-08-04 11:31:42 +0900

    :art: Refactoring AppMain

--- frontend/src/components/main/AppMain.vue
+++ frontend/src/components/main/AppMain.vue
@@ -2,7 +2,10 @@
   <v-main>
     <v-container fluid>
       <v-layout>
-        <v-flex offset-lg3 lg6>
+        <v-flex v-if="isLoggedIn" offset-lg3 lg6>
+          <router-view></router-view>
+        </v-flex>
+        <v-flex v-if="!isLoggedIn">
           <router-view></router-view>
         </v-flex>
       </v-layout>
@@ -12,7 +15,10 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import { mapGetters } from "vuex";
 
-@Component
+@Component({
+  computed: { ...mapGetters(["isLoggedIn"]) },
+})
 export default class AppMain extends Vue {}
 </script>

402b0f37d149561e59f7a652a3dcaf571bca2abe
김준호 <tuvmxm@naver.com>
2020-08-04 11:32:44 +0900

    :art: Add cover component

--- frontend/src/views/Cover.vue
+++ frontend/src/views/Cover.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container mt-5">
-    <div class="row">
+    <div class="row py-5">
       <ServiceDescription />
       <RssImage />
     </div>
+    <hr />
+    <ServiceStep />
+    <hr />
     <UserInformation />
   </div>
 </template>
@@ -13,13 +16,15 @@ import { Vue, Component } from "vue-property-decorator";
 import ServiceDescription from "@/components/cover/ServiceDescription.vue";
 import RssImage from "@/components/cover/RssImage.vue";
 import UserInformation from "@/components/cover/UserInformation.vue";
+import ServiceStep from "@/components/cover/ServiceStep.vue";
 
 @Component({
   components: {
     ServiceDescription,
     RssImage,
-    UserInformation
-  }
+    UserInformation,
+    ServiceStep,
+  },
 })
 export default class Cover extends Vue {}
 </script>

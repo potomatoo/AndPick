257f0e768dc83e0bca55027e6a4db577349da1e6
ydh2338 <ydh2338@naver.com>
2020-08-17 01:35:26 +0900

    :sparkles: find rss by category name like

--- backend/src/main/java/com/ssafy/model/repository/RssRepository.java
+++ backend/src/main/java/com/ssafy/model/repository/RssRepository.java
@@ -3,10 +3,15 @@ package com.ssafy.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.model.dto.Category;
 import com.ssafy.model.dto.Rss;
+import com.ssafy.model.dto.TagCount;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 @Repository
 public interface RssRepository extends JpaRepository<Rss, Long> {
@@ -17,4 +22,9 @@ public interface RssRepository extends JpaRepository<Rss, Long> {
 	public Rss findOneByRssUrl(String rssUrl);
 
 	public Rss findOneByRssId(long rssId);
+
+	@Modifying
+	@Query(value = "select rss_id,rss_url,rss.category_id,rss_name from SSAFYDB.rss join SSAFYDB.category on SSAFYDB.rss.category_id = SSAFYDB.category.category_id where SSAFYDB.category.category_name like :category_name", nativeQuery = true)
+	public List<Rss> findByCategoryNameLike(String category_name);
+
 }

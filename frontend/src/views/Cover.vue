35d0b12f73eb30257d693b429c983112885960ba
김준호 <tuvmxm@naver.com>
2020-08-19 15:33:04 +0900

    :art: Change

--- frontend/src/views/Cover.vue
+++ frontend/src/views/Cover.vue
@@ -2,8 +2,9 @@
   <div>
     <div class="back row py-5">
       <ServiceDescription />
+      <span @click="test" class="mdi mdi-arrow-down-drop-circle-outline"></span>
     </div>
-    <ServiceStep />
+    <ServiceStep class="service-position" />
     <UserInformation />
   </div>
 </template>
@@ -23,10 +24,26 @@ import ServiceStep from "@/components/cover/ServiceStep.vue";
     ServiceStep
   }
 })
-export default class Cover extends Vue {}
+export default class Cover extends Vue {
+  test() {
+    const element = document.querySelector(".service-position");
+    if (element) {
+      const elementTop = element.getBoundingClientRect().top;
+      const abTop = window.pageYOffset + elementTop;
+      window.scrollTo({ top: abTop - 65, behavior: "smooth" });
+    }
+  }
+}
 </script>
 
 <style>
+.mdi-arrow-down-drop-circle-outline {
+  font-size: 50px;
+  position: absolute;
+  left: 50%;
+  top: 520px;
+  cursor: pointer;
+}
 .back {
   background-size: 100% 100%;
   background-color: #1e847f;
